b5bb6ae71e23f9b551f2fb1ab0a554bde70ca367
stevie <122219518+avocado-beans@users.noreply.github.com>
2024-04-18 17:49:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import time
-import requests
-while True:
-    response = requests.get("https://cathequeen.onrender.com")
+from flask import Flask, request, render_template, redirect, url_for, session, jsonify
+
+app = Flask(__name__)  
+
+@app.route('/<ratings>', methods =["GET", "POST"])
+def save(ratings):
+    if request.method == "POST":
+        file = open('test.txt', 'w')
+        file.writelines(ratings)
+        file.close()
+        return 'done'
+    if request.method == "GET":
+        file = open('test.txt', 'r')
+        return file.readlines()
